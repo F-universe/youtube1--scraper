25cde312190a65c35fe6e8b015d3ea9160c80354
F-universe <fabioberberithings@yahoo.com>
2024-10-04 10:47:13 +0200

    Update youtube.py

--- youtube.py
+++ youtube.py
@@ -1,38 +1,78 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service  # Importa la classe Service
-from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time
+from bs4 import BeautifulSoup
 
-# Configura Flask
+# Set up Flask
 app = Flask(__name__)
 
-# Configura il percorso del ChromeDriver
+# Set the ChromeDriver path
 chromedriver_path = r'C:\Users\fabio\Downloads\chromedriver-win64\chromedriver-win64\chromedriver.exe'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', data=None)
 
 @app.route('/search', methods=['POST'])
 def search():
-    # Ottieni la parola inserita dall'utente nel form
-    parola = request.form['parola']
+    # Get the word entered by the user in the form
+    keyword = request.form['keyword']
     
-    # Configura il webdriver di Selenium utilizzando Service
+    # Set up Selenium WebDriver using Service
     service = Service(executable_path=chromedriver_path)
     driver = webdriver.Chrome(service=service)
     
-    # Vai su YouTube e cerca la parola
-    search_url = f"https://www.youtube.com/results?search_query={parola}"
+    # Go to YouTube and search for the keyword
+    search_url = f"https://www.youtube.com/results?search_query={keyword}"
     driver.get(search_url)
-    
-    # Lascia il browser aperto per 5 secondi per vedere i risultati
+
+    # Add a pause to load the initial results
     time.sleep(5)
     
-    # Chiude il browser
+    # Scroll down to load more videos (increased number of scrolls)
+    for _ in range(10):  # Increase the number of scrolls
+        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+        time.sleep(2)  # Wait a bit longer to load more content
+    
+    # Wait until the video elements are visible
+    WebDriverWait(driver, 10).until(
+        EC.presence_of_all_elements_located((By.CSS_SELECTOR, "ytd-video-renderer"))
+    )
+
+    # Get the HTML of the page
+    page_html = driver.page_source
+    soup = BeautifulSoup(page_html, 'html.parser')
+    
+    # Find all video elements (ytd-video-renderer)
+    video_elements = soup.find_all('ytd-video-renderer')
+
+    # List to store video details
+    video_data = []
+
+    # Iterate over each video to extract information
+    for video in video_elements:
+        # Extract the video title
+        title_tag = video.find('a', id='video-title')
+        title = title_tag['title'] if title_tag else 'N/A'
+
+        # Extract the video URL
+        url = f"https://www.youtube.com{title_tag['href']}" if title_tag else '#'
+
+        # Add the video to the list of collected data
+        video_data.append({
+            'title': title,
+            'url': url
+        })
+    
+    # Close the browser
     driver.quit()
     
-    return redirect('/')
+    # Pass the data to the HTML template
+    return render_template('index.html', data=video_data)
 
 if __name__ == '__main__':
     app.run(debug=True)
